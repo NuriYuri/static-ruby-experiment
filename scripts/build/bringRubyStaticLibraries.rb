de2e5ffb1785cff6b0e689f68ef27aa0dec8c6be
Nuri Yuri <yuri@pokemonworkshop.com>
2024-12-12 21:28:10 +0100

    Improve bringRubyStaticLibraries

--- scripts/build/bringRubyStaticLibraries.rb
+++ scripts/build/bringRubyStaticLibraries.rb
@@ -1,14 +1,11 @@
 #!/usr/bin/env ruby
 
-ruby_dir = ENV['RUBY_DIR']
-raise 'run "source setup.sh" before running this script' unless ruby_dir
+ruby_install_dir = ENV['RUBY_INSTALL_DIR']
+raise 'run "source setup.sh" before running this script' unless ruby_install_dir
 
 Dir.chdir(ENV['STATIC_RUBY_TOP_LEVEL_DIR'])
 
-print "Ruby build sub directory: [build]"
-ruby_sub_directory = STDIN.gets.chomp.then { |v| v.empty? ? 'build' : v }
-
-all_unique_libraries = Dir[File.join(ruby_dir, ruby_sub_directory, '**', '*.a')].uniq { |v| File.basename(v) }
+all_unique_libraries = Dir[File.join(File.dirname(ruby_install_dir), '**', '*.a')].uniq { |v| File.basename(v) }
 all_unique_libraries.each do |filename|
   target_filename = File.basename(filename).then { |v| v.start_with?('lib') ? v : "lib#{v}" }
   puts "Copying #{target_filename}"
